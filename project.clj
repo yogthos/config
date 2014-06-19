0703f20187f48acf24255045229b33932032c3a8
Yogthos <dmitri.sotnikov@gmail.com>
2014-06-19 00:10:24 -0400

    updated project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject edn-config "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject edn-config "0.1"
+  :description "library for managing environment variables in Clojure using EDN configuration files"
+  :url "https://github.com/yogthos/edn-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
