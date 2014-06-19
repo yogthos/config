7314bccd9953b59ddebe933374d6bfc32ade227b
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2014-06-19 00:55:50 -0400

    fixed typo
    Update README.md
    
    Update README.md
    
    updated to use environ

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject edn-config "0.1"
+(defproject edn-config "0.2"
   :description "library for managing environment variables in Clojure using EDN configuration files"
   :url "https://github.com/yogthos/edn-config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [environ "1.0.0"]])
