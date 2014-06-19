e8a983eeef5868170f9b628e584a189c5f9baefc
Yogthos <dmitri.sotnikov@gmail.com>
2014-06-19 00:23:42 -0400

    added tests, code cleanup

--- test/edn_config/core_test.clj
+++ test/edn_config/core_test.clj
@@ -1,7 +1,24 @@
 (ns edn-config.core-test
   (:require [clojure.test :refer :all]
-            [edn-config.core :refer :all]))
+            [edn-config.core :as e]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn refresh-ns []
+  (ns-unalias *ns* 'e)
+  (remove-ns 'edn-config.core)
+  (dosync (alter @#'clojure.core/*loaded-libs* disj 'edn-config.core))
+  (require '[edn-config.core :as e]))
+
+(defn refresh-env []
+  (eval `(do (refresh-ns) e/env)))
+
+(deftest test-env
+  (testing "env variables"
+    (is (= (:user e/env) (System/getenv "USER")))
+    (is (= (:java-arch e/env) (System/getenv "JAVA_ARCH"))))
+  (testing "system properties"
+    (is (= (:user-name e/env) (System/getProperty "user.name")))
+    (is (= (:user-country e/env) (System/getProperty "user.country"))))
+  (testing "env file"
+    (spit "test/config.edn" (prn-str {:foo "bar"}))
+    (let [env (refresh-env)]
+      (is (= (:foo env) "bar")))))
