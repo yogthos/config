03a07b539b614bd7986621fe21ed1a8fb12d4a11
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-28 08:54:28 -0500

    bumped up version/environ dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject luminus/config "0.3"
+(defproject luminus/config "0.4"
   :description "library for managing configuration using environment variables/EDN configuration files"
   :url "https://github.com/luminus-framework/config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [environ "1.0.1"]])
+                 [environ "1.0.2"]])
