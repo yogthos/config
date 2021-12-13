81750c8fdb946050a8074f14ab03c8b226536b1a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2021-12-12 20:34:46 -0500

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject yogthos/config "1.1.8"
+(defproject yogthos/config "1.1.9"
   :description "library for managing configuration using environment variables/EDN configuration files"
   :url "https://github.com/yogthos/config"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.1"]])
